2440d0cfd1adf5a3ee30eb7a454295ba31889755
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2022-12-30 14:51:54 +0100

    feat: add `files` module

---
+++ files.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'https://deno.land/std@0.167.0/path/mod.ts'
+
+export async function* files(dir: string): AsyncIterableIterator<string> {
+  const dirents = Deno.readDir(dir)
+  
+  for await (const dirent of dirents) {
+    const res = resolve(dir, dirent.name)
+
+    if (dirent.isDirectory)
+      yield* files(res)
+    else
+      yield res
+  }
+}
