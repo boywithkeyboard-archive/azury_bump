a4d3a016456b1b0b8f875ec509e986bad400bd74
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-02-07 23:13:57 +0100

    feat: add `cdn.skypack.dev` registry

---
+++ registries/cdn.skypack.dev.ts
@@ -0,0 +1,51 @@
+import { Registry } from '../Registry.ts'
+
+export default new Registry({
+  name: 'cdn.skypack.dev',
+  getName(url) {
+    const packageName = url
+      .split('/')[1]
+      .split('@')[0]
+
+    if (packageName.length > 0)
+      return packageName
+
+    return url.split('/')[1] + '/' + url.split('/')[2].split('@')[0]
+  },
+  getCurrentVersion(url) {
+    const scopedPackage = url.split('/')[1].split('@')[0].length === 0
+
+    return scopedPackage ? (
+      url.split('/')[2].split('@')[1]
+    ) : (
+      url.split('/')[1].split('@')[1]
+    )
+  },
+  async getNextVersion(name) {
+    const res = await fetch(`https://registry.npmjs.org/${name}`)
+      
+    if (!res.ok)
+      throw new Error()
+
+    return (await res.json())['dist-tags'].latest
+  },
+  getPackageUrl(name) {
+    return `https://npmjs.com/package/${name}`
+  },
+  getCurrentVersionUrl(name, version) {
+    return `https://npmjs.com/package/${name}/v/${version}`
+  },
+  getNextVersionUrl(name, version) {
+    return `https://npmjs.com/package/${name}/v/${version}`
+  },
+  async getRepository(name) {
+    const res = await fetch(`https://registry.npmjs.org/${name}`)
+      
+    if (!res.ok)
+      return undefined
+
+    const json = await res.json()
+
+    return json.versions[json['dist-tags'].latest].repository.url.replace('git+', '').replace('.git', '')
+  }
+})
