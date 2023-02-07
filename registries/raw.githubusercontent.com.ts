665aefc8bfd7afd73b8cffe30cd114ccdff98dbf
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-02-07 23:46:52 +0100

    feat: add `raw.githubusercontent.com` registry

---
+++ registries/raw.githubusercontent.com.ts
@@ -0,0 +1,28 @@
+import { Registry } from '../Registry.ts'
+
+export default new Registry({
+  name: 'raw.githubusercontent.com',
+  getName(url) {
+    return url.split('/')[1] + '/' + url.split('/')[2] // org/repo
+  },
+  getCurrentVersion(url) {
+    return url.split('/')[3]
+  },
+  async getNextVersion(name) {
+    const res = await fetch(`https://api.github.com/repos/${name}/releases`)
+      
+    if (!res.ok)
+      throw new Error()
+
+    return (await res.json())[0].tag_name
+  },
+  getCurrentVersionUrl(name, version) {
+    return `https://github.com/${name}/releases/tag/${version}`
+  },
+  getNextVersionUrl(name, version) {
+    return `https://github.com/${name}/releases/tag/${version}`
+  },
+  getRepository(name) {
+    return `https://github.com/${name}`
+  }
+})
